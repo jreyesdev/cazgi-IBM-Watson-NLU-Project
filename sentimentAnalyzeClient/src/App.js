ff445fcdcfbef7210196d52e09cdea9685c426ff
jreyesdev <djrm1204@gmail.com>
2021-08-27 00:00:54 -0400

    url en app.js

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
 
   sendForSentimentAnalysis = () => {
     this.setState({sentiment:true});
-    let url = 'http://localhost:8080';
+    let url = '';
 
     if(this.state.mode === "url") {
       url = url+"/url/sentiment?url="+document.getElementById("textinput").value;
@@ -59,7 +59,7 @@ class App extends React.Component {
   sendForEmotionAnalysis = () => {
 
     this.setState({sentiment:false});
-    let url = 'http://localhost:8080';
+    let url = '';
     if(this.state.mode === "url") {
       url = url+"/url/emotion?url="+document.getElementById("textinput").value;
     } else {
